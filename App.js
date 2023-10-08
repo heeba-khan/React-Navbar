da18ab89cd847df7418300756e404024ecdd5a64
Heeba Khan <heebak45@gmail.com>
2023-10-08 10:29:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from "react"
+import Navbar from "./Navbar"
+import Pricing from "./pages/Pricing"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import {Route,Routes} from "react-router-dom"
+
+function App(){
+  // let component
+  
+    return (
+
+      <div>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        
+      </div>
+      
+      )
+}
+
+export default App
